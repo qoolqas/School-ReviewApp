c723269647921892514c006d5cc947ffddade82c
Qoolqas <qoolqas@gmail.com>
2020-08-20 13:44:13 +0700

    collapsing toolbar(fix)

--- app/src/main/java/com/id/schoolreview/ui/home/DetailActivity.java
+++ app/src/main/java/com/id/schoolreview/ui/home/DetailActivity.java
@@ -1,10 +1,13 @@
 package com.id.schoolreview.ui.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.id.schoolreview.R;
 import com.id.schoolreview.pojo.DataSchool;
 
@@ -17,6 +20,16 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         dataSchool = getIntent().getParcelableExtra("data");
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle(dataSchool.getNama());
+
+        collapsingToolbarLayout.setCollapsedTitleTextColor(
+                ContextCompat.getColor(this, R.color.white));
+        collapsingToolbarLayout.setExpandedTitleColor(
+                ContextCompat.getColor(this, R.color.transparent));
 
         Log.d("test", Objects.requireNonNull(dataSchool).getNama());
 
